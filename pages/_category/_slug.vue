41205b0c916f12debb221bd228301fa5e65e1bdf
Pedro Jiménez <hola@pedrojimenez.info>
2019-11-18 17:04:15 +0100

    páginas dinamicas

---
+++ pages/_category/_slug.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>{{ $route.params.slug }}</p>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
---
+++ pages/_category/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>{{ $route.params.category }}</p>
+  </div>
+</template>
