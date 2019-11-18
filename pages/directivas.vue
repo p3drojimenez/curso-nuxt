123a58abd2046ed300c2f49b6a87b0ed933811d4
Pedro Jiménez <hola@pedrojimenez.info>
2019-11-18 17:31:11 +0100

    propiedades computadas

--- pages/directivas.vue
+++ pages/directivas.vue
@@ -12,6 +12,10 @@
     <p>Nombre:</p>
     <input v-model="name" type="text"/>
     {{ name }}
+    {{ nameReverser }}
+    <span class="tag is-warning" v-if="error.length != 0">
+      {{ error }}
+    </span>
     <button v-on:click="saludo" class="button is-info">
       Saluda
     </button>
@@ -24,7 +28,22 @@ export default {
       message: "Hola OpenWebinars",
       num: 5,
       items: [1,2,3,4,5,6],
-      name: ''
+      name: '',
+      error: ''
+    }
+  },
+  computed: {
+    nameReverser() {
+      return this.name.split('').reverse().join('')
+    }
+  },
+  watch: {
+    name() {
+      if(this.name.length > 5) {
+        this.error = "Nombre demasiado largo"
+      } else {
+        this.error = ''
+      }
     }
   },
   methods: {
