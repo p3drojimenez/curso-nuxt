a0473d0af3ff5c0700ecc14bfbe2c80b07aef44a
Pedro Jiménez <hola@pedrojimenez.info>
2019-11-18 16:20:59 +0100

    componente RestaurantCard

---
+++ components/RestaurantCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card" style="width:400px;">
+    <div class="card-image">
+      <figure class="image">
+        <img src="~/assets/images/restaurant.jpg" alt="Pizza Restaurant" />
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-4 restaurant-name">
+            Restaurant Name
+          </p>
+          <div class="columns">
+            <div class="column">
+              <span class="is-category tag">Category</span>
+            </div>
+            <div class="column has-text-right">
+              <button class="button is-info">0</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.Phasellus nec
+        iaculis mauris.<a href="">more info</a>.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.restaurant-name {
+  font-size: 30px;
+  color: #8ae6ff;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="container">
     <section class="section">
-      <h1 class="title is-1">
-        FoodAdvisor
-      </h1>
-      <button class="button is-info">
-        Enter
-      </button>
+      <RestaurantCard />
     </section>
   </div>
 </template>
 
 <script>
+import RestaurantCard from "../components/RestaurantCard"
 export default {
+  components: {
+    RestaurantCard
+  }
 }
 </script>
 
